c1b7df938a1f9715e821a1b5baec62e992ba4b7b
Steven Paulo Perando <pauloperando@gmail.com>
2024-10-13 14:46:03 +0800

    Update EmployeePayrollSystem.java

--- EmployeePayrollSystem.java
+++ EmployeePayrollSystem.java
@@ -9,20 +9,35 @@ public class EmployeePayrollSystem {
         Employee2 ee2 = new Employee2("E002", "Bob Hann", 30, 150);
         
         Scanner myObj = new Scanner(System.in);
-        System.out.println("Please choose which employee would you like to calculate the salary");
-        System.out.println("Employee 1 \nEmployee 2 ");
+        System.out.println("Please choose which employee information you would like to view");
+        System.out.println("Alice Go \nBob Hann");
         String input = myObj.nextLine();
+        System.out.println();
         
-        switch(input) {
-            case "1":
-                System.out.println();
-                System.out.print(ee1.calculateSalary());
-                break;
+        if (input.equalsIgnoreCase("Alice Go")) {
+            System.out.println(ee1.displayEmployeeDetails(160));
+            System.out.println("Would you like to calculate the salary? YES/NO");
+            String input2 = myObj.nextLine();
+            System.out.println();
+                if (input2.equalsIgnoreCase("YES")) {
+                    System.out.print("Salary: " + ee1.calculateSalary(160));
+                } 
+                else {
+                    System.out.println("Okay! The program will be terminated");
+                }
+        }
                 
-            case "2":
-                System.out.println();
-                System.out.print(ee2.calculateSalary());
-                break;
+        if (input.equalsIgnoreCase("Bob Hann")) {
+            System.out.println(ee2.displayEmployeeDetails(170));
+            System.out.println("Would you like to calculate the salary? YES/NO");
+            String input3 = myObj.nextLine();
+            System.out.println();
+                if (input3.equalsIgnoreCase("YES")) {
+                    System.out.print("Salary: " + ee2.calculateSalary(170));
+                } 
+                else {
+                    System.out.println("Okay! The program will be terminated");
+                }
         }
     }  
 }
