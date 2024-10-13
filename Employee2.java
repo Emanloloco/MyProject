323db99af94add8f1467dbb99e54feb87e3bbee1
Steven Paulo Perando <pauloperando@gmail.com>
2024-10-13 14:46:35 +0800

    Update Employee2.java

--- Employee2.java
+++ Employee2.java
@@ -1,25 +1,27 @@
 package Classes;
 
 public class Employee2 {
-    String ID;
-    String Name;
-    double Wage;
-    double HoursWorked;
+    String ID2;
+    String Name2;
+    double Wage2;
+    double HoursWorked2;
     
     // Constructor
     public Employee2(String id2, String name2, double wage2, double hoursWorked2) {
-        this.ID = id2;
-        this.Name = name2;
-        this.Wage = wage2;
-        this.HoursWorked = hoursWorked2;
+        this.ID2 = id2;
+        this.Name2 = name2;
+        this.Wage2 = wage2;
+        this.HoursWorked2 = hoursWorked2;
     }
-    public double calculateSalary(){
+    
+    public String displayEmployeeDetails(double hoursWorked2) {
+        return "ID:" + ID2 + "\nName: " + Name2 + "\nWage: " + Wage2 + "\nHoursWorked: " + hoursWorked2;
+    }
+    
+    public double calculateSalary(double hoursWorked2){
         double salary = 0;
-        String ID;
-        String Name;
-        double Wage = 0;
-        double HoursWorked = 0;
-        salary = Wage * HoursWorked;
+        salary = Wage2 * hoursWorked2;
+        System.out.println("ID: " + ID2 + "\nName: " + Name2 + "\nWage: " + Wage2 + "\nHoursWorked: " + hoursWorked2);
         return salary;
     }
 }
