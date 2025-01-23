b3223fd7f42cea3239f2f1828734cc00dc7181c6
eestrella318 <ebe0302@students.uc-bcf.edu.ph>
2025-01-23 12:27:58 +0800

    Update RolePlayingGame.java

--- RolePlayingGame.java
+++ RolePlayingGame.java
@@ -1,8 +1,14 @@
 package roleplayinggame;
+import Classes.Enemy;
+
 
 public class RolePlayingGame {
-
     public static void main(String[] args) {
-        
-    }  
+       Enemy goblin = new Enemy("Goblin", 200, 50, 30);
+       Enemy ogre = new Enemy("Ogre", 250, 40, 25);
+       Enemy centaur = new Enemy("Centaur", 225, 45, 45);
+       Enemy orc = new Enemy("Warlock", 240, 50, 40);
+       Enemy dragon = new Enemy("Alduin", 500, 70, 50);
+       
+    }
 }
