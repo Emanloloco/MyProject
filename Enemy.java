338dc4506f1f4ddee46ac38586092e3701806131
Steven Paulo Perando <pauloperando@gmail.com>
2025-01-28 01:22:50 +0800

    Update Enemy.java

--- Enemy.java
+++ Enemy.java
@@ -1,21 +1,25 @@
 package Classes;
 
+import Classes.Character;
+
 public class Enemy {
 
     String name;
-    String job;
     int health;
+    int maxHealth;
     int damage;
-    int speed;
-    int exp;
+    public int speed;
+    public int exp;
 
-    public Enemy(String name, String job, int health, int damage, int exp) {
+    public Enemy(String name, int health, int damage, int speed, int exp) {
         this.name = name;
         this.health = health;
+        this.maxHealth = health;
         this.damage = damage;
         this.speed = speed;
+        this.exp = exp;
     }
-    
+
     // Getter for enemy attributes
     public String getName() {
         return name;
@@ -35,8 +39,20 @@ public class Enemy {
 
     public void takeDamage(int amount) {
         this.health -= amount;
-        if (this.health <= 0) {
-            System.out.println("You have defeated the " + name + " !");
+        if (this.health < 0) {
+            this.health = 0;
         }
+    }
+    
+    public void setHealth(int health) {
+        this.health = health;
+    }
+    
+    public int getMaxHealth(Enemy enemy) {
+        return maxHealth;
+    }
+    
+    public void resetEnemy() {
+        this.health = this.maxHealth;
     }
 }
